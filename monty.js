7639c3d858d69dbca454b2ed9cbc109a4c1383b4
Carl Eiserman <carleiserman@gmail.com>
2017-03-18 08:32:24 +0100

    Feature: Added monty.js - second attempt

---
+++ monty.js
@@ -0,0 +1,19 @@
+/*
+    Solution to the Monty Hall Door Problem
+
+    Author: Carl Eiserman
+    Website: https://github.com/carl-eis
+
+*/
+
+/*====================================================
+  Imports
+====================================================*/
+
+var Random = require("random-js");
+var random = new Random(Random.engines.mt19937().autoSeed());
+
+/*====================================================
+  Globals
+====================================================*/
+
